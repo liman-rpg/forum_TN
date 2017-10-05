b81fb527c5072b83ba1f7aee47eeaffb83590c97
R.G <liman.vita@gmail.com>
2017-10-05 16:39:31 +0300

    Answers Controller #create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+  before_action :load_question, only: :create
+
   def new
     @answer = Answer.new
   end
@@ -6,4 +8,22 @@ class AnswersController < ApplicationController
   def edit
     @answer = Answer.find(params[:id])
   end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+  private
+    def load_question
+      @question = Question.find(params[:question_id])
+    end
+
+    def answer_params
+      params.require(:answer).permit(:body)
+    end
 end
