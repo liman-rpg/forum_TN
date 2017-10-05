f50b4160d99e1b8a1a8f87fb7abc32bf760f88a8
R.G <liman.vita@gmail.com>
2017-10-05 15:49:06 +0300

    Add Answers Controller + #new

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer)   { create(:answer) }
+
+  describe "GET #new" do
+    before { get :new, params: { question_id: question.id } }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "GET #edit"
+  describe "POST #create"
+  describe "POST #update"
+  describe "DELETE #destroy"
+end
