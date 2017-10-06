60b2c6492773347a583add839e68ba82056865e2
R.G <liman.vita@gmail.com>
2017-10-06 17:00:26 +0300

    Acceptance spec question#index

---
+++ spec/acceptance/questions/show_index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Show questions_path page', %q{
+  The user can view a list of questions,
+  As a user or a guest,
+  The client can see questions list
+} do
+
+  given!(:questions) { create_list(:question, 3) }
+
+  scenario 'Show #index' do
+    visit questions_path
+
+    expect(page).to have_content 'Questions list:'
+    expect(page).to have_link 'RspecQuestionTitle1'
+    expect(page).to have_link 'RspecQuestionTitle2'
+    expect(page).to have_link 'RspecQuestionTitle3'
+    expect(current_path).to eq questions_path
+  end
+end
