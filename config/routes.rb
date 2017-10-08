2d301aa126e926af2af6e5b0e6eb6baa2710bcfe
R.G <liman.vita@gmail.com>
2017-10-08 18:51:22 +0300

    Acceptance User log_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :questions do
     resources :answers, shallow: true
   end
 
-  root "questions#index"
+  root to: "questions#index"
 end
