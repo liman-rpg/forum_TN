32c5ff70a6566a6359d2135bc06b54150d2f2d88
R.G <liman.vita@gmail.com>
2017-10-08 20:10:46 +0300

    Question create (Acceptance)

---
+++ spec/acceptance/questions/create_question_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+feature 'Create question', %q{
+  In order to ask a question
+  As a authenticate user
+  I want to create question
+} do
+  given(:user) { create(:user) }
+
+  scenario 'User try create a question' do
+    visit root_path
+    sign_in(user)
+    click_on "Add Question"
+
+    fill_in 'Title', with: 'New Question Title'
+    fill_in 'Body', with: 'New Question Body'
+    click_on 'Create'
+
+    expect(page).to have_content "Your question successfully created."
+    expect(page).to have_content "New Question Title"
+    expect(page).to have_content "New Question Body"
+  end
+
+  scenario 'Non authenticated user try create a question' do
+    visit root_path
+
+    expect(page).to_not have_content 'Add Question'
+  end
+end
