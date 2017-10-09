3874a66997f44a76d9ffa8d40d22ed4f38bd7b9e
R.G <liman.vita@gmail.com>
2017-10-09 15:25:36 +0300

    Check for output of validation errors for quetions

--- spec/acceptance/questions/create_question_spec.rb
+++ spec/acceptance/questions/create_question_spec.rb
@@ -7,18 +7,45 @@ feature 'Create question', %q{
 } do
   given(:user) { create(:user) }
 
-  scenario 'User try create a question' do
-    visit root_path
-    sign_in(user)
-    click_on "Add Question"
+  describe 'Authenticate user' do
+    before do
+      sign_in(user)
+      visit root_path
+    end
 
-    fill_in 'Title', with: 'New Question Title'
-    fill_in 'Body', with: 'New Question Body'
-    click_on 'Create'
+    scenario 'to create the correct question' do
+      click_on "Add Question"
 
-    expect(page).to have_content "Your question successfully created."
-    expect(page).to have_content "New Question Title"
-    expect(page).to have_content "New Question Body"
+      fill_in 'Title', with: 'New Question Title'
+      fill_in 'Body', with: 'New Question Body'
+      click_on 'Create'
+
+      expect(page).to have_content "Your question successfully created."
+      expect(page).to have_content "New Question Title"
+      expect(page).to have_content "New Question Body"
+    end
+
+    scenario ' to create a non-valid question' do
+      click_on "Add Question"
+
+      fill_in 'Title', with: nil
+      fill_in 'Body', with: 'New Question Body'
+      click_on 'Create'
+
+      expect(page).to have_content "Titlecan't be blank"
+
+      fill_in 'Title', with: 'New Question Title'
+      fill_in 'Body', with: nil
+      click_on 'Create'
+
+      expect(page).to have_content "Body can't be blank"
+
+      fill_in 'Title', with: 'New Question Title'
+      fill_in 'Body', with: 'smal'
+      click_on 'Create'
+
+      expect(page).to have_content "Body smalis too short (minimum is 5 characters)"
+    end
   end
 
   scenario 'Non authenticated user try create a question' do
