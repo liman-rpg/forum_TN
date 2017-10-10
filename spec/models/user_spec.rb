33fbabd08e7f592871a90e1ce526c38e7e28e6a4
R.G <liman.vita@gmail.com>
2017-10-10 15:40:19 +0300

    Add spec for User #author_of?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe User, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
 
   it { should validate_presence_of :name }
+
+  describe 'author_of?' do
+    let(:author)       { create(:user) }
+    let(:another_user) { create(:user) }
+    let(:object)       { create(:answer, user: author) }
+
+    it 'true/false' do
+      expect(author.author_of?(object)).to eq true
+      expect(another_user.author_of?(object)).to eq false
+    end
+  end
 end
