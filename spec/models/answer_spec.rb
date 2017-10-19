7dfb444f16f4a3e18d5d586536ce19a097d820a3
R.G <liman.vita@gmail.com>
2017-10-19 09:56:53 +0300

    Add column BEST for Answers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to(:question) }
+
   it { should validate_presence_of :body }
   it { should validate_length_of(:body).is_at_least(5) }
+
+  it { should have_db_column(:best).of_type(:boolean).with_options(default: false) }
 end
