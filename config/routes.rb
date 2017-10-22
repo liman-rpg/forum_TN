185b2864683f1315c214ea6934d1ac56c840cd3b
R.G <liman.vita@gmail.com>
2017-10-22 17:03:45 +0300

    Mini Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :questions do
-    resources :answers, shallow: true
+    resources :answers, shallow: true do
+      post 'set_as_best', on: :member, as: 'best'
+    end
   end
-
-  post '/answers/set_as_best/:id', to: 'answers#set_as_best', as: 'best_answer'
 
   root to: "questions#index"
 end
