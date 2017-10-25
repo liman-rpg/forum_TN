1e6337f066c7460a47eebda0b0f60cee430aac15
R.G <liman.vita@gmail.com>
2017-10-25 16:47:30 +0300

    Only author can delete him attachment in answer

---
+++ spec/acceptance/answers/delete_files_to_answer_spec.rb
@@ -0,0 +1,43 @@
+require 'acceptance_helper'
+
+feature 'Delete files to answer', %q{
+  In order to remove illustrate my answer
+  As an answer's author
+  I'd like to be able to remove files
+  } do
+    given(:user)        { create(:user) }
+    given(:other_user)  { create(:user) }
+    given(:question)    { create(:question) }
+    given!(:answer)     { create(:answer, :with_attachment, question: question, user: user) }
+
+    scenario 'Authenticate user as author the answer can delete his files', js: true do
+      sign_in(user)
+      visit question_path(question)
+
+      within ("#answer-id-#{answer.id}") do
+        expect(page).to have_link 'acceptance_helper.rb', href: /uploads\/attachment\/file\/\d+\/\w+_helper.rb/
+
+        click_on 'Delete file'
+      end
+
+      expect(page).to have_content "Your attachment was successfully destroy."
+      expect(page).to_not have_link 'acceptance_helper.rb', href: /uploads\/attachment\/file\/\d+\/\w+_helper.rb/
+    end
+
+    scenario 'Authenticate user as not author the answer can not delete other user files' do
+      sign_in(other_user)
+      visit question_path(question)
+
+      within ("#answer-id-#{answer.id}") do
+        expect(page).to_not have_link 'Delete file'
+      end
+    end
+
+    scenario 'Not authenticate user can not delete files' do
+      visit question_path(question)
+
+      within ("#answer-id-#{answer.id}") do
+        expect(page).to_not have_link 'Delete file'
+      end
+    end
+  end
