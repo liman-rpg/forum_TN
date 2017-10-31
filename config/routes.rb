e1742e90a1b30d9cc709244a23f56f03d7ed64f4
R.G <liman.vita@gmail.com>
2017-10-31 15:28:21 +0300

    Add concerns :votable (Routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :questions do
-    post 'vote_up', on: :member, as: 'vote_up'
-    post 'vote_down', on: :member, as: 'vote_down'
-    post 'vote_cancel', on: :member, as: 'vote_cancel'
-    resources :answers, shallow: true do
+  concern :votable do
+    member do
+      post 'vote_up'
+      post 'vote_down'
+      post 'vote_cancel'
+    end
+  end
+
+  resources :questions, concerns: :votable do
+    resources :answers, shallow: true, concerns: :votable do
       post 'set_as_best', on: :member, as: 'best'
     end
   end
