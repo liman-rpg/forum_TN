a2db10d6fc5d96697b5d76ce8670814c05f4ba36
R.G <liman.vita@gmail.com>
2017-11-15 15:57:38 +0300

    Change controller through params[:commentable]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_question
+  before_action :load_commentable
 
   def create
-    @comment = @question.comments.new(comment_params.merge(user_id: current_user.id))
+    @comment = @commentable.comments.new(comment_params.merge(user_id: current_user.id))
     @comment.save
   end
 
   private
+
+  def commentable
+    params[:commentable]
+  end
+
+  def load_commentable
+    @commentable = commentable.classify.constantize.find(params["#{commentable}_id"])
+  end
 
   def comment_params
     params.require(:comment).permit(:body)
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,42 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  let(:question) { create(:question) }
+  let(:question)  { create(:question) }
+  let(:answer)    { create(:answer) }
 
   describe "POST #create" do
     sign_in_user
 
-    context 'with valid attributes' do
-      let(:create_valid_comment) { post :create, params: { comment: attributes_for(:comment), question_id: question, format: :js } }
+    context "commentable 'question'" do
+      context 'with valid attributes' do
+        let(:create_valid_comment) { post :create, params: { comment: attributes_for(:comment), question_id: question, commentable: 'question', format: :js } }
 
-      it "save new comment in database" do
-        expect{ create_valid_comment }.to change(question.comments, :count).by(+1)
+        it "save new comment in database" do
+          expect{ create_valid_comment }.to change(question.comments, :count).by(+1)
+        end
+
+        it "associated with the user" do
+          expect { create_valid_comment }.to change(@user.comments, :count).by(+1)
+        end
+
+        it 'render create.js' do
+          create_valid_comment
+          expect(response).to render_template :create
+        end
       end
 
-      it "associated with the user" do
-        expect { create_valid_comment }.to change(@user.comments, :count).by(+1)
-      end
+      context 'with invalid attributes' do
+        let(:create_invalid_comment) { post :create, params: { comment: { body: nil }, question_id: question, commentable: 'question', format: :js } }
 
-      it 'render create.js' do
-        create_valid_comment
-        expect(response).to render_template :create
+        it "don't save new comment in database" do
+          expect{ create_invalid_comment }.to_not change(Comment, :count)
+        end
+
+        it "not associated with the user" do
+          expect { create_invalid_comment }.to_not change(@user.comments, :count)
+        end
+
+        it 'render create.js' do
+          create_invalid_comment
+          expect(response).to render_template :create
+        end
       end
     end
 
-    context 'with invalid attributes' do
-      let(:create_invalid_comment) { post :create, params: { comment: { body: nil }, question_id: question, format: :js } }
+    context "commentable 'answer'" do
+      context 'with valid attributes' do
+        let(:create_valid_comment) { post :create, params: { comment: attributes_for(:comment), answer_id: answer, commentable: 'answer', format: :js } }
 
-      it "don't save new comment in database" do
-        expect{ create_invalid_comment }.to_not change(Comment, :count)
+        it "save new comment in database" do
+          expect{ create_valid_comment }.to change(answer.comments, :count).by(+1)
+        end
+
+        it "associated with the user" do
+          expect { create_valid_comment }.to change(@user.comments, :count).by(+1)
+        end
+
+        it 'render create.js' do
+          create_valid_comment
+          expect(response).to render_template :create
+        end
       end
 
-      it "not associated with the user" do
-        expect { create_invalid_comment }.to_not change(@user.comments, :count)
-      end
+      context 'with invalid attributes' do
+        let(:create_invalid_comment) { post :create, params: { comment: { body: nil }, answer_id: answer, commentable: 'answer', format: :js } }
 
-      it 'render create.js' do
-        create_invalid_comment
-        expect(response).to render_template :create
+        it "don't save new comment in database" do
+          expect{ create_invalid_comment }.to_not change(Comment, :count)
+        end
+
+        it "not associated with the user" do
+          expect { create_invalid_comment }.to_not change(@user.comments, :count)
+        end
+
+        it 'render create.js' do
+          create_invalid_comment
+          expect(response).to render_template :create
+        end
       end
     end
   end
