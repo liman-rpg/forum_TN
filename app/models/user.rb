8c3ab26635bfa3d4b8edade2bcddaf9f688999ec
R.G <liman.vita@gmail.com>
2017-11-26 02:08:52 +0300

    Add method .from_omniauth (model User)

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,27 @@ class User < ApplicationRecord
   def author_of?(object)
     id == object.user_id
   end
+
+  def self.from_omniauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+
+    if user
+      user.create_authorization(auth)
+    else
+      name     = Devise.friendly_token[0, 5]
+      password = Devise.friendly_token[0, 20]
+      user     = User.create!(name: name, email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+
+    user
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
+  end
 end
