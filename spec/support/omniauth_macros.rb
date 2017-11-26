b25a126c3ddaa99ac196321da5e199219f2e03b8
R.G <liman.vita@gmail.com>
2017-11-26 22:04:21 +0300

    Create module OmniauthMacros

---
+++ spec/support/omniauth_macros.rb
@@ -0,0 +1,16 @@
+module OmniauthMacros
+  def mock_auth_hash(provider)
+    OmniAuth.config.mock_auth[provider] = OmniAuth::AuthHash.new({
+      provider: provider.to_s,
+      uid: '12345',
+      info: {
+        email: 'mock@auth.com',
+        name: 'mockuser'
+      }
+    })
+  end
+
+  def mock_auth_invalid_hash(provider)
+    OmniAuth.config.mock_auth[provider] = :invalid_credentials
+  end
+end
