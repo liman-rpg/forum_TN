a84b0f1febc66799bb31ea474f298f9769ec7b47
R.G <liman.vita@gmail.com>
2017-12-19 11:35:00 +0300

    Create API '/questions'

---
+++ spec/api/v1/questions_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe 'Questions Api' do
+  describe 'GET #index' do
+    context 'unauthorized' do
+      it 'return 401 status if there is no access_token' do
+        get "/api/v1/questions", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'return 401 status if access_token is invalid' do
+        get '/api/v1/questions', params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let!(:questions)   { create_list(:question, 3) }
+      let(:question)     { questions.first }
+      let!(:answer)      { create(:answer, question: question) }
+
+      before { get '/api/v1/questions', params: { format: :json, access_token: access_token.token } }
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      it 'returns list questions' do
+        expect(response.body).to have_json_size(3)
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+        end
+      end
+
+      context 'answers' do
+        it 'included in question object' do
+          expect(response.body).to have_json_size(1).at_path("0/answers")
+        end
+
+        %w(id body created_at updated_at).each do |attr|
+          it "contains #{attr}" do
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/answers/0/#{attr}")
+          end
+        end
+      end
+    end
+  end
+end
