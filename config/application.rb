3447bb47d9447dd868e75f3e2aa0dd7d43874ae2
R.G <liman.vita@gmail.com>
2017-12-20 09:19:19 +0300

    Add gem Sereializer + create question_collection_serializer

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module ForumTn
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    ActiveModelSerializers.config.adapter = :json
   end
 end
