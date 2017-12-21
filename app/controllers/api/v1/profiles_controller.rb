28895a58789423cf361c5930b2b76adcc71b8ef3
R.G <liman.vita@gmail.com>
2017-12-21 11:29:34 +0300

    Add authorization for Api::Profiles::Controller

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
   def me
+    authorize current_resource_owner, :api?
     respond_with current_resource_owner
   end
 
   def index
+    authorize User.new, :api?
     respond_with User.where.not(id: current_resource_owner.id)
   end
 end
