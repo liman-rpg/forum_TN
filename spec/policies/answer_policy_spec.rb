2b6b11032b766fd25b3ab693ace4bbe1052096a7
R.G <liman.vita@gmail.com>
2017-12-21 11:03:51 +0300

    Add authorization for Api::Answer::Controller

--- spec/policies/answer_policy_spec.rb
+++ spec/policies/answer_policy_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe AnswerPolicy do
     let(:user) { nil }
 
     it { is_expected.to permit_actions([:index, :show]) }
-    it { is_expected.to forbid_actions([:destroy, :vote, :set_as_best, :update]) }
+    it { is_expected.to forbid_actions([:destroy, :vote, :set_as_best, :update, :api]) }
     it { is_expected.to forbid_new_and_create_actions }
     it { is_expected.to forbid_edit_and_update_actions }
   end
@@ -19,14 +19,14 @@ RSpec.describe AnswerPolicy do
   context 'being an author' do
     let(:answer) { create :answer, user: user }
 
-    it { is_expected.to permit_actions([:index, :show, :destroy]) }
+    it { is_expected.to permit_actions([:index, :show, :destroy, :api]) }
     it { is_expected.to forbid_actions([:vote, :set_as_best]) }
     it { is_expected.to permit_new_and_create_actions }
     it { is_expected.to permit_edit_and_update_actions }
   end
 
   context 'being not an author' do
-    it { is_expected.to permit_actions([:index, :show, :vote]) }
+    it { is_expected.to permit_actions([:index, :show, :vote, :api]) }
     it { is_expected.to forbid_actions([:destroy, :set_as_best]) }
     it { is_expected.to permit_new_and_create_actions }
     it { is_expected.to forbid_edit_and_update_actions }
@@ -35,7 +35,7 @@ RSpec.describe AnswerPolicy do
   context "being an author answer's question " do
     subject { AnswerPolicy.new(question.user, answer) }
     let(:question) { create :question, user: user }
-    let(:answer) { create :answer, question: question }
+    let(:answer)   { create :answer, question: question }
 
     it { is_expected.to permit_action(:set_as_best) }
   end
