173a56de1da23029c13e5215c07cabec7ebbea3a
R.G <liman.vita@gmail.com>
2017-12-21 10:03:43 +0300

    Create API 'Create Answer'

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -61,4 +61,69 @@ describe 'Answers Api' do
       end
     end
   end
+
+  describe 'POST #create' do
+    let(:user)         { create(:user) }
+    let(:question)     { create(:question) }
+    let(:answer)       { create(:answer, question: question) }
+    let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+
+    context 'unauthorized' do
+      it 'return 401 status if there is no access_token' do
+        post "/api/v1/questions/#{question.id}/answers", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'return 401 status if access_token is invalid' do
+        post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:answer_last) { Answer.last }
+
+      context 'with valid attributes' do
+        let(:create_valid_answer) { post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: attributes_for(:answer) } }
+
+        it 'save answer in database' do
+          expect { create_valid_answer }.to change(Answer, :count).by(+1)
+        end
+
+        context 'after create' do
+          before { create_valid_answer }
+
+          it 'returns 201 status' do
+            expect(response).to be_success
+          end
+
+          %w(id body created_at updated_at).each do |attr|
+            it "contains #{ attr }" do
+              expect(response.body).to be_json_eql(answer_last.send(attr.to_sym).to_json).at_path("answer/#{ attr }")
+            end
+          end
+
+          it 'check attributes' do
+            answer_last.reload
+
+            expect(answer_last.body).to eq "Rspec Body Answer"
+            expect(answer_last.user_id).to eq user.id
+          end
+        end
+      end
+
+      context 'with invalid attributes' do
+        let(:create_invalid_answer) { post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: attributes_for(:invalid_answer) } }
+
+        it 'do not save answer in database' do
+          expect { create_invalid_answer }.to_not change(Answer, :count)
+        end
+
+        it 'returns 422 status' do
+          create_invalid_answer
+          expect(response.status).to eq 422
+        end
+      end
+    end
+  end
 end
